1e9b9ea18ecd562a2304c23d84cc7f95201cef11
lynaBoukari <il_boukari@esi.dz>
2022-05-28 22:13:35 +0100

    add banners

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ import {Flex, Box, Text , Button} from '@chakra-ui/react' ;
 
 const Banner =({purpose, buttonText,linkName,imageURL,title2, title1,desc1,desc2 }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    <Image src ={imageURL} width={500} height={300} alt="banner"/>
+    <Image  width="500" height="300" alt="banner" src={imageURL}/>
     <Box>
       <Text color="gray.500" fontSize="sm" fontWeight="medium"> {purpose}</Text>
       <Text fontSize="3xl" fontWeight="bold"> {title1} <br/> {title2}</Text>
       <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700" > {desc1} <br/> {desc2}</Text>      
-      <Button fontSize="xl" bg="blue.300" color="white">
+      <Button fontSize="xl" bg="blue.300" >
           <Link href={linkName} >{buttonText}</Link>
       </Button>
       </Box>
@@ -20,18 +20,31 @@ const Banner =({purpose, buttonText,linkName,imageURL,title2, title1,desc1,desc2
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World !</h1>
-      <Banner purpose= "RENT A HOME" 
+    <Box>
+     
+      <Banner
+        purpose= "RENT A HOME" 
         title1="Rental Homes for"
         title2="Everyone"
+        imageURL='/images/house1.jpg'
         desc1="Explore Apartments, Villas, Homes"
         desc2="and more" 
         buttonText ="Explore Renting"
         linkName="/search?purpose=for-rent"
-        imageURL="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        
       />
-      <Banner purpose= {" For rent"}/>
-    </div>
+        <Banner
+        purpose= "BUY A HOME" 
+        title1="Find, Buy & Own Your"
+        title2="Dream Home"
+        imageURL='/images/house2.jpg'
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more" 
+        buttonText ="Explore Buying"
+        linkName="/search?purpose=for-sale"
+        
+      />
+     
+    </Box>
   )
 }
